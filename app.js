dbdfee8e97010dcadd519de3861c1d8622f19370
aoife <amulvenna10@qub.ac.uk>
2024-04-15 22:02:14 +0100

    adds data from updated database design. starts to use authentication

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
-const axios = require("axios");
+
+const mysql = require('mysql2');
+
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+const cookieParser = require('cookie-parser');
+const sessions = require('express-session');
+
+const oneHour = 1000 * 60 * 60 * 1;
+
+app.use(cookieParser());
+
+app.use(sessions({
+   secret: "mypokemon12",
+   saveUninitialized: true,
+   cookie: { maxAge: oneHour },
+   resave: false
+}));
 
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'pokemon',
+    port: '8889'
+});
+
+// db.connect((err) => {
+//     if (err) throw err;
+// });
+
+
+// renders main view, pokemon logo, and background images
 app.get('/', (req, res) => {
 
     res.render('main', { logoPath: 'Pokemon_Logo.png', backgroundPath: 'background-2.png' });
@@ -15,30 +44,43 @@ app.get('/', (req, res) => {
 });
 
 app.get('/view-cards', (req, res) => {
+    const query = `
+    SELECT pc.pokemon_name, pt.pokemon_type_name, pc.url_img, pc.hp, ps.pokemon_stage_name, pc.attack, pc.rarity, pc.weakness, psn.pokemon_set_name, pc.evolve_from 
+    FROM pokemon_card pc 
 
-    let ep = `http://localhost:4000/pokemon/view-cards`;
+    INNER JOIN pokemon_type pt ON pt.pokemon_type_id = pc.pokemon_type_id
+    INNER JOIN pokemon_stage ps ON ps.pokemon_stage_id = pc.pokemon_stage_id
+    INNER JOIN pokemon_set psn ON psn.pokemon_set_id = pc.pokemon_set_id;`
 
-    axios.get(ep).then((response) => {
-        let bdata = response.data;
-        res.render('view-cards', { titletext: 'view pokemon', bdata, logoPath: 'Pokemon_Logo.png' });
-    });
+    db.query(query, (err, results) => {
 
-});
+        if (err) throw err;
 
-app.get('/details', (req, res) => {
-    let card_id = req.query.card;
-    let endp = `http://localhost:4000/pokemon/${card_id}`;
-
-    axios.get(endp).then((response) => {
-        res.send(response.data);
+        res.render('view-cards', { cards: results, logoPath: 'Pokemon_Logo.png' });
     });
 });
 
+
+// renders home view 
 app.get('/home', (req, res) => {
-    res.render('home', { logoPath: 'Pokemon_Logo.png' });
+    const sessionobj = req.session;
+   
+    if(sessionobj.authen){
+        const uid = sessionobj.authen;
+        const user = `SELECT * FROM user WHERE user_id = "${uid}"`;
+
+        db.query(user, (err, row)=>{
+            const firstrow = row[0];
+            res.render('home', { logoPath: 'Pokemon_Logo.png', userdata:firstrow});
+        })
+   
+    } else{
+        res.send("Acccess Denied");
+    }
+
 });
 
-
+// renders login
 app.get('/login', (req, res) => {
 
     res.render('login', { logoPath: 'Pokemon_Logo.png', backgroundPath: 'background-2.png' });
@@ -46,88 +88,41 @@ app.get('/login', (req, res) => {
 });
 
 
+
+
+app.post('/', (req, res) => {
+    const username = req.body.usernameField;
+// const password = req.body.passwordField;
+
+    const checkuser = `SELECT * FROM user WHERE username = "${username}"`;
+    db.query(checkuser, (err, rows) => {
+        if (err) throw err;
+        const numRows = rows.length;
+
+        if (numRows > 0) {
+            const sessionobj = req.session;  
+            sessionobj.authen = rows[0].id; 
+            res.redirect('/home');
+
+        } else {
+            res.redirect('/');
+
+        }
+
+    });
+});
+
+// renders sign up
 app.get('/sign-up', (req, res) => {
 
     res.render('sign-up', { logoPath: 'Pokemon_Logo.png', backgroundPath: 'background-2.png' });
 
 });
 
+app.post('/sign-up', (req,res) => {
 
-
-app.get('/populate-one-card/:setId/:localId', (req, res) => {
-    let set_id = req.params.setId
-    let localId = req.params.localId
-
-    const ep = 'https://api.tcgdex.net/v2/en/sets/' + set_id + '/' + localId
-
-    let cardDataReq = axios.get(ep);
-  
-    cardDataReq.then((response) => {
-        const cardData = response.data
-        // console.log(cardData);
-
-
-        insertData =
-            {
-                name: cardData.name,
-                type: "TODO: Change",
-                url_img: cardData.image,
-                set_name: cardData.set.name,
-                hp: cardData.hp,
-                stage: cardData.stage,
-                attack: "TODO: Change",
-                local_id: cardData.localId,
-                rarity: cardData.rarity,
-                description: cardData.description,
-                weakness: "TODO: Change",
-                retreat: cardData.retreat,
-                set_id: cardData.set.id,
-                illustrator: cardData.illustrator,
-                evolve_from: cardData.evolveFrom
-            }
-
-        let endpoint = `http://localhost:4000/pokemon/add`;
-
-        const config = {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        }
-
-        axios.post(endpoint, insertData, config).then((response) => {
-            let insertedid = response.data.respObj.id;
-            let resmessage = response.data.respObj.message;
-            res.send(`${resmessage}. INSERTED DB id ${insertedid}`);
-        }).catch((err) => {
-            // console.log(err.message);
-        });
-    });
 });
 
-
-app.get('/populate-one-card-set/:setId', (req, res) => {
-    let set_id = req.params.setId
-
-    let setDataReq = axios.get(`https://api.tcgdex.net/v2/en/sets/` + set_id);
-
-    setDataReq.then((response) => {
-        const setData = response.data
-
-        for (let i = 0; i < setData.cards.length; i++) {
-            cardData = setData.cards[i];
-            const ep = 'http://localhost:3000/populate-one-card/' + set_id + '/' + cardData.localId;
-            let cardDataReq = axios.get(ep);
-
-            setDataReq.then((response) => {
-                console.log("success")
-            });
-        }
-
-        res.send(`Set: ${setData.name} - INSERTED ALL CARDS TO DB`);
-    });
-});
-
-
-const server = app.listen(PORT, () => {
-    console.log(`API started on port ${server.address().port}`);
+app.listen(3000, () => {
+    console.log('Server on port 3000');
 });
