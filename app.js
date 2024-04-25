87f278515c0274ebc653729e90393fcb12b65c18
aoife <amulvenna10@qub.ac.uk>
2024-04-25 17:31:05 +0100

    adds filter for rarity

--- app.js
+++ app.js
@@ -869,38 +869,92 @@ app.post('/delete-account', (req, res) => {
 });
 
 app.get('/guest-view-cards/sort', (req, res) => {
-    
-    const sort = req.query.sort;
-const order = req.query.value === 'asc' ? 'ASC' : 'DESC';
 
-    let filterQuery = ` SELECT pc.pokemon_card_id, pc.pokemon_name, pt.pokemon_type_name, pc.url_img, pc.hp, ps.pokemon_stage_name, pc.attack, r.rarity_name, pc.weakness, psn.pokemon_set_name, pc.evolve_from 
+    const sort = req.query.sort;
+    const order = req.query.value === 'asc' ? 'ASC' : 'DESC';
+
+    let sortQuery = ` SELECT pc.pokemon_card_id, pc.pokemon_name, pt.pokemon_type_name, pc.url_img, pc.hp, ps.pokemon_stage_name, pc.attack, r.rarity_name, pc.weakness, psn.pokemon_set_name, pc.evolve_from 
     FROM pokemon_card pc 
     INNER JOIN pokemon_type pt ON pt.pokemon_type_id = pc.pokemon_type_id
     INNER JOIN pokemon_stage ps ON ps.pokemon_stage_id = pc.pokemon_stage_id
     INNER JOIN pokemon_set psn ON psn.pokemon_set_id = pc.pokemon_set_id
     INNER JOIN rarity r ON r.rarity_id = pc.rarity_id ORDER BY ${sort} ${order}`;
 
-    db.query(filterQuery, (err, result) => {
+    db.query(sortQuery, (err, result) => {
         if (err) throw err;
-    
-            const stageQuery = `SELECT * FROM pokemon_stage`;
-            db.query(stageQuery, (err, stageResults) => {
-                if (err) throw err;
 
-                const rarityQuery = `SELECT * FROM rarity`;
-                db.query(rarityQuery, (err, rarityResults) => {
+        const stageQuery = `SELECT * FROM pokemon_stage`;
+        db.query(stageQuery, (err, stageResults) => {
+            if (err) throw err;
+
+            const rarityQuery = `SELECT * FROM rarity`;
+            db.query(rarityQuery, (err, rarityResults) => {
+                if (err) throw err;
+                const typeQuery = `SELECT * FROM pokemon_type`;
+                db.query(typeQuery, (err, typeResults) => {
                     if (err) throw err;
-                    const typeQuery = `SELECT * FROM pokemon_type`;
-                    db.query(typeQuery, (err, typeResults) => {
+                    const setQuery = `SELECT * FROM pokemon_set`;
+                    db.query(setQuery, (err, setResults) => {
                         if (err) throw err;
-                        const setQuery = `SELECT * FROM pokemon_set`;
-                        db.query(setQuery, (err, setResults) => {
-                            if (err) throw err;
-                            res.render('guest-view-cards', { cards: result, sess_obj: false, currentPage: req.path, stageResults, rarityResults, typeResults, setResults });
-                        });
-                    })
+                        res.render('guest-view-cards', { cards: result, sess_obj: false, currentPage: req.path, stageResults, rarityResults, typeResults, setResults });
+                    });
                 })
             })
+        })
+    });
+});
+
+app.get('/guest-view-cards/filter-rarity', (req, res) => {
+    let sess_obj = req.session;
+    sess_obj.authen = false;
+    const selectedRarities = req.query.rarity;
+    console.log('Selected rarities:', selectedRarities)
+    let query = `
+    SELECT pc.pokemon_card_id, pc.pokemon_name, pt.pokemon_type_name, pc.url_img, pc.hp, 
+           ps.pokemon_stage_name, pc.attack, r.rarity_name, pc.weakness, 
+           psn.pokemon_set_name, pc.evolve_from 
+    FROM pokemon_card pc 
+    INNER JOIN pokemon_type pt ON pt.pokemon_type_id = pc.pokemon_type_id
+    INNER JOIN pokemon_stage ps ON ps.pokemon_stage_id = pc.pokemon_stage_id
+    INNER JOIN pokemon_set psn ON psn.pokemon_set_id = pc.pokemon_set_id
+    INNER JOIN rarity r ON r.rarity_id = pc.rarity_id`;
+    if (!Array.isArray(selectedRarities)) {
+        console.log('Selected Rarity' , selectedRarities);
+      let rarityFilter=` WHERE r.rarity_name IN ('${selectedRarities}') `;
+      query += rarityFilter;
+      }
+    else if (selectedRarities.length > 1) {
+        let rarityFilter = `  WHERE r.rarity_name IN (`;
+        for (let i = 0; i < selectedRarities.length; i++) {
+          rarityFilter += `'${selectedRarities[i]}'`;
+          if (i < selectedRarities.length -1) {
+            rarityFilter += `, `;
+          }
+        }
+        rarityFilter += `)`;
+        query += rarityFilter;
+      }
+      console.log('Generated SQL Query:', query);
+    db.query(query, (err, results) => {
+        if (err) throw err;
+        const stageQuery = `SELECT * FROM pokemon_stage`;
+        db.query(stageQuery, (err, stageResults) => {
+            if (err) throw err;
+
+            const rarityQuery = `SELECT * FROM rarity`;
+            db.query(rarityQuery, (err, rarityResults) => {
+                if (err) throw err;
+                const typeQuery = `SELECT * FROM pokemon_type`;
+                db.query(typeQuery, (err, typeResults) => {
+                    if (err) throw err;
+                    const setQuery = `SELECT * FROM pokemon_set`;
+                    db.query(setQuery, (err, setResults) => {
+                        if (err) throw err;
+                        res.render('guest-view-cards', { cards: results, sess_obj: false, currentPage: req.path, stageResults, rarityResults, typeResults, setResults });
+                    });
+                })
+            })
+        })
     });
 });
 
